3ae60e766ef71b54123c960081a2e00e757d4066
Nicholas Labarre <bitbucket@ndlr.xyz>
2018-09-04 21:39:06 -0400

    cleaned up challenge 1

--- src/java/src/test/xyz/ndlr/Set1Test.java
+++ src/java/src/test/xyz/ndlr/Set1Test.java
@@ -10,7 +10,7 @@ public class Set1Test {
     @Test
     public void challenge1() {
         ConvertHelper convertHelper = new ConvertHelper();
-        Challenge1 challenge1 = new Challenge1(convertHelper);
+        Challenge1 challenge1 = new Challenge1();
 
         String input =
                 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
