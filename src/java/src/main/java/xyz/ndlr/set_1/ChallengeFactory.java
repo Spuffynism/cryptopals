4a0e88e48a858ab99b0569a1dfc752b6f7ccbb39
Nicholas Labarre <bitbucket@ndlr.xyz>
2018-09-09 13:09:20 -0400

    implemented ChallengeFactory Challenges creation

--- src/java/src/main/java/xyz/ndlr/set_1/ChallengeFactory.java
+++ src/java/src/main/java/xyz/ndlr/set_1/ChallengeFactory.java
@@ -1,4 +1,34 @@
 package xyz.ndlr.set_1;
 
+import xyz.ndlr.utill.ConvertionHelper;
+
 public class ChallengeFactory {
+    private ConvertionHelper convertionHelper = null;
+
+    private ConvertionHelper getConvertionHelper() {
+        if (convertionHelper == null)
+            convertionHelper = new ConvertionHelper();
+
+        return convertionHelper;
+    }
+
+    public Challenge2 getChallenge2() {
+        return new Challenge2();
+    }
+
+    public Challenge3 getChallenge3() {
+        return new Challenge3();
+    }
+
+    public Challenge4 getChallenge4() {
+        return new Challenge4(getChallenge3(), getConvertionHelper());
+    }
+
+    public Challenge5 getChallenge5() {
+        return new Challenge5();
+    }
+
+    public Challenge6 getChallenge6() {
+        return new Challenge6(getChallenge4(), getChallenge5(), getConvertionHelper());
+    }
 }
