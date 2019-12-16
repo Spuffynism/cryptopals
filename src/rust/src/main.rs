4043580b4e435a31a87b38bef17b5628811a6b91
Nicholas Labarre <bitbucket@ndlr.xyz>
2019-12-15 20:02:46 -0500

    remove rust main body

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,11 +1,6 @@
-extern crate rustc_serialize;
 extern crate core;
+extern crate rustc_serialize;
 
 mod set1;
 
-fn main() {
-	let numerator:f64 = 330_323.0;
-	let denominator:f64 = 152_680.0 * (152_680.0 - 1.0);
-	
-	println!("{}", (numerator / denominator).to_string());
-}
+fn main() {}
