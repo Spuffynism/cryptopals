caf87fc21a5332aa2ec068205716684a58d81c42
Nicholas Labarre <bitbucket@ndlr.xyz>
2019-12-17 21:01:49 -0500

    rust - pass values by reference instead of by copying rust - complete non-working first rendition of set 1 challenge 6

--- src/rust/src/set1.rs
+++ src/rust/src/set1.rs
@@ -3,39 +3,39 @@ extern crate hex;
 use std::char;
 use std::fs;
 use std::str;
+use std::f32;
 
 use rustc_serialize::base64::{STANDARD, ToBase64};
 
 use xor;
 
-static ALPHABET: [char; 68] = [
+static ALPHABET: [char; 72] = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    ',', ';', ':', '.', ' ', '\''
+    ',', ';', ':', '.', ' ', '\'', '\n', '\\', '/', '"',
 ];
 
 pub fn hex_string_to_bytes(hex_input: &str) -> Vec<u8> {
     hex::decode(hex_input).unwrap()
 }
 
-pub fn hex_to_bytes(hex_input: Vec<u8>) -> Vec<u8> {
-    hex_string_to_bytes(String::from_utf8(hex_input).unwrap().as_str())
+pub fn hex_to_bytes(hex_input: &Vec<u8>) -> Vec<u8> {
+    hex_string_to_bytes(String::from_utf8(hex_input.as_slice().to_vec()).unwrap().as_str())
 }
 
-pub fn hex_fixed_xor(hex_input: Vec<u8>, hex_key: Vec<u8>) -> Vec<u8> {
+pub fn hex_fixed_xor(hex_input: &Vec<u8>, hex_key: &Vec<u8>) -> Vec<u8> {
     assert_eq!(hex_input.len(), hex_key.len());
 
     let input: Vec<u8> = hex_to_bytes(hex_input);
     let key: Vec<u8> = hex_to_bytes(hex_key);
 
-    return xor::fixed_xor(input, key);
+    return xor::fixed_xor(&input, &key);
 }
 
-pub fn find_single_byte_xor(hex_input: Vec<u8>) -> (char, f32, Vec<u8>) {
-    let mut input: Vec<u8> = hex_to_bytes(hex_input);
+pub fn find_single_byte_xor(input: &Vec<u8>) -> (char, f32, Vec<u8>) {
     let mut best_key: char = 'a';
     let mut best_score: f32 = -1_f32;
     let mut best_result: Vec<u8> = vec![];
@@ -59,6 +59,10 @@ pub fn find_single_byte_xor(hex_input: Vec<u8>) -> (char, f32, Vec<u8>) {
     return (best_key, best_score, best_result);
 }
 
+pub fn hex_find_single_byte_xor(hex_input: &Vec<u8>) -> (char, f32, Vec<u8>) {
+    find_single_byte_xor(&hex_to_bytes(hex_input))
+}
+
 fn calculate_human_resemblance_score(input: &Vec<u8>) -> f32 {
     let mut human_characters_count = 0;
 
@@ -78,7 +82,7 @@ fn find_most_human(candidates: Vec<Vec<u8>>) -> (char, f32, Vec<u8>, Vec<u8>) {
     let mut best_candidate: Vec<u8> = vec![];
 
     for candidate in candidates {
-        let (character, score, xored) = find_single_byte_xor(candidate.clone());
+        let (character, score, xored) = hex_find_single_byte_xor(&candidate);
 
         if score > best_score {
             best_key = character;
@@ -91,19 +95,74 @@ fn find_most_human(candidates: Vec<Vec<u8>>) -> (char, f32, Vec<u8>, Vec<u8>) {
     return (best_key, best_score, best_result, best_candidate);
 }
 
-fn break_repeating_key_xor(cipher: Vec<u8>,
-                           key_alphabet: Vec<u8>,
-                           min_key_length: u8,
-                           max_key_length: u8,
-                           max_blocks_average: u8,
-                           best_guesses_count: u8) -> (Vec<u8>, Vec<u8>) {
+fn break_repeating_key_xor(cipher: &Vec<u8>,
+                           key_alphabet: Vec<u8>, // TODO: Make use of this
+                           min_key_size: u8,
+                           max_key_size: u8,
+                           max_blocks_average: u8,  // TODO: Make use of this
+                           best_guesses_count: u8  // TODO: Make use of this
+    ) -> (Vec<u8>, Vec<u8>) {
     let mut key: Vec<u8> = vec![];
-    let mut deciphered: Vec<u8> = vec![];
+    let mut best_key_size: u8 = 0;
+    let mut best_normalized_hamming_distance = std::f32::MAX;
+
+    for key_size in &[min_key_size, max_key_size] {
+        let mut first = cipher[..*key_size as usize].to_vec();
+        let mut second = cipher[(key_size + 1) as usize..(key_size * 2 + 1) as usize].to_vec();
+
+        while first.len() != second.len() {
+            if first.len() < second.len() {
+                first.push(0);
+            } else {
+                second.push(0);
+            }
+        }
+
+        let normalized_hamming_distance = normalized_hamming_distance_in_bits(&first.to_vec(),
+                                                                              &second.to_vec());
+
+        if normalized_hamming_distance < best_normalized_hamming_distance {
+            best_key_size = *key_size;
+            best_normalized_hamming_distance = normalized_hamming_distance;
+        }
+    }
+
+    let mut rows = Vec::new();
+    for _i in 0..best_key_size {
+        rows.push(Vec::new());
+    }
+
+    let rows_length = (cipher.len() as f32 / best_key_size as f32).ceil() as i32;
+
+    // [1,2,3, 4,5,6, 7,8,9]
+    let mut i = 0;
+    while i < rows_length {
+        for j in 0..best_key_size {
+            if (i * rows_length + j as i32) as usize >= cipher.len() {
+                break;
+            }
+
+            rows[j as usize].push(cipher[(i * rows_length + j as i32) as usize]);
+        }
+        i += 1;
+    }
+
+    let mut key = Vec::with_capacity(rows.len());
+    for row in rows.iter() {
+        let (best_key, best_score, best_result) = find_single_byte_xor(row);
+        key.push(best_key as u8);
+    }
+
+    let mut deciphered: Vec<u8> = xor::fixed_key_xor(&cipher, &key);
 
     return (key, deciphered);
 }
 
-fn hamming_distance_of_bits(from: Vec<u8>, to: Vec<u8>) -> u32 {
+fn normalized_hamming_distance_in_bits(from: &Vec<u8>, to: &Vec<u8>) -> f32 {
+    (hamming_distance_in_bits(from, to) as f32) / (to.len() as f32)
+}
+
+fn hamming_distance_in_bits(from: &Vec<u8>, to: &Vec<u8>) -> u32 {
     assert_eq!(from.len(), to.len());
     let mut distance: u32 = 0;
 
@@ -144,8 +203,8 @@ mod tests {
 
     #[test]
     fn challenge1() {
-        let input: Vec<u8> =
-            vs!("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+        let input: &Vec<u8> =
+            &vs!("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
         let expected: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
         assert_eq!(hex_to_bytes(input).to_base64(STANDARD), expected)
@@ -153,22 +212,22 @@ mod tests {
 
     #[test]
     fn challenge2() {
-        let input: Vec<u8> = vs!("1c0111001f010100061a024b53535009181c");
-        let key: Vec<u8> = vs!("686974207468652062756c6c277320657965");
-        let expected: Vec<u8> = vs!("746865206b696420646f6e277420706c6179");
+        let input: &Vec<u8> = &vs!("1c0111001f010100061a024b53535009181c");
+        let key: &Vec<u8> = &vs!("686974207468652062756c6c277320657965");
+        let expected: &Vec<u8> = &vs!("746865206b696420646f6e277420706c6179");
 
         assert_eq!(hex_fixed_xor(input, key), hex_to_bytes(expected))
     }
 
     #[test]
     fn challenge3() {
-        let input: Vec<u8> =
-            vs!("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
-        let expected = vs!("Cooking MC's like a pound of bacon");
+        let input: &Vec<u8> =
+            &vs!("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+        let expected = &vs!("Cooking MC's like a pound of bacon");
 
-        let (key, score, xored_result) = find_single_byte_xor(input);
+        let (key, score, xored_result) = hex_find_single_byte_xor(input);
 
-        assert_eq!(xored_result, expected);
+        assert_eq!(xored_result, *expected);
     }
 
     #[test]
@@ -188,9 +247,9 @@ mod tests {
     #[test]
     fn challenge5() {
         let content =
-            vs!("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
-        let key = vs!("ICE");
-        let expected = vs!(
+            &vs!("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
+        let key = &vs!("ICE");
+        let expected = &vs!(
             "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
 
         assert_eq!(xor::fixed_key_xor(content, key), hex_to_bytes(expected));
@@ -209,16 +268,24 @@ mod tests {
         ];
 
         for (from, to, expected_distance) in test_cases {
-            assert_eq!(hamming_distance_of_bits(vs!(from), vs!(to)), expected_distance);
+            assert_eq!(hamming_distance_in_bits(&vs!(from), &vs!(to)), expected_distance);
         }
     }
 
     #[test]
     fn challenge6() {
         let content = fs::read_to_string("./resources/6.txt").expect("Can't read file.");
-        let lines = content
+        let decoded_content: &Vec<u8> = &content
             .split("\n")
             .map(|line| base64::decode(line).unwrap())
-            .collect::<Vec<Vec<u8>>>();
+            .collect::<Vec<Vec<u8>>>()
+            .iter()
+            .fold(Vec::new(), |acc, line| [acc.as_slice(), line.as_slice()].concat());
+
+        let (key, deciphered) = break_repeating_key_xor(decoded_content, vec![], 2, 50,
+                                                        4, 3);
+
+        println!("{:?}", String::from_utf8(key));
+        println!("{:?}", String::from_utf8(deciphered));
     }
 }
